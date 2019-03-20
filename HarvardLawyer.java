07e1492a01f7b7baae3d87993b80086064d41f45
mossflower1437 <46542732+mossflower1437@users.noreply.github.com>
2019-03-20 00:24:29 -0700

    Add files via upload

---
+++ HarvardLawyer.java
@@ -0,0 +1,18 @@
+
+public class HarvardLawyer extends Lawyer {
+	 
+	public void showSalary() {
+	        System.out.println("My salary is $48,000.");
+	}
+	
+	public void showVacation() {
+        System.out.println("I receive 3 weeks and 3 days vacation.");
+    }
+	
+	  public void applyForVacation() {
+	        System.out.println("Use the pink vacation form.");
+	        System.out.println("Use the pink vacation form.");
+	        System.out.println("Use the pink vacation form.");
+	        System.out.println("Use the pink vacation form.");
+	  }
+}
---
+++ Janitor.java
@@ -0,0 +1,18 @@
+
+public class Janitor extends Employee {
+	 public void showVacation() {
+	        System.out.println("I receive 5 days vacation.");
+	 }
+	 
+	 public void showSalary() {
+	        System.out.println("My salary is $30,000.");
+	 }
+	 
+	 public void showHours() {
+	        System.out.println("I work 80 hours per week.");
+	 }
+	 
+	 public void clean() {
+	        System.out.println("Workin' for the man.");
+	 }
+}
